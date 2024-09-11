6dd7dc52a86b640b8fdad362aa07ed717d1eefa1
Hossam-Allam <h.refaie@stu.khas.edu.tr>
2024-09-11 18:39:56 +0300

    added move methods for knight

--- lib/pieces/knight.rb
+++ lib/pieces/knight.rb
@@ -1,7 +1,42 @@
+require_relative "pawn"
+# Knight class
 class Knight
-  attr_reader :symbol
+  include MoveMapper
+  attr_reader :symbol, :color
 
   def initialize(color)
+    @color = color
     @symbol = color == "black" ? "♘" : "♞"
+  end
+
+  def move(coordinates, _board)
+    start, destination = parse_coordinates(coordinates)
+
+    possible_moves = filter_valid_moves(knight_moves(start))
+
+    possible_moves.include?(destination)
+  end
+
+  private
+
+  def knight_moves(position)
+    moves = [
+      [2, 1], [2, -1], [-2, 1], [-2, -1],
+      [1, 2], [1, -2], [-1, 2], [-1, -2]
+    ]
+
+    x, y = position
+
+    moves.map do |dx, dy|
+      [x + dx, y + dy]
+    end
+  end
+
+  def filter_valid_moves(moves)
+    valid_range = (0..7)
+
+    moves.select do |x, y|
+      valid_range.include?(x) && valid_range.include?(y)
+    end
   end
 end
