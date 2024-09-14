ec759bd868a7956d5dab85df9c29b93d702278c7
Hossam-Allam <h.refaie@stu.khas.edu.tr>
2024-09-14 09:14:53 +0300

    added methods to check for potential checks

--- lib/pieces/king.rb
+++ lib/pieces/king.rb
@@ -22,7 +22,12 @@ class King
     end
   end
 
-  def check?(board = nil)
+  def check?(board)
+    return true if check_vertical(board)
+    return true if check_horizontal(board)
+    return true if check_diagonals(board)
+    return true if check_knights(board)
+
     false
   end
 
@@ -37,5 +42,84 @@ class King
 
   def path_clear?(_start, destination, board)
     board[destination[0]][destination[1]].nil? || board[destination[0]][destination[1]].color != color
+  end
+
+  def check_vertical(board)
+    # Check up and down from the king's current position
+    row, col = location
+
+    [-1, 1].each do |row_step|
+      new_row = row + row_step
+      while new_row.between?(0, 7)
+        piece = board[new_row][col]
+        return false if piece && piece.color == color # Blocked by own piece
+        if piece
+          # Check if the piece can attack vertically (i.e., a rook or queen)
+          return piece.is_a?(Rook) || piece.is_a?(Queen)
+        end
+
+        new_row += row_step
+      end
+    end
+    false
+  end
+
+  def check_horizontal(board)
+    # Check left and right from the king's current position
+    row, col = location
+
+    [-1, 1].each do |col_step|
+      new_col = col + col_step
+      while new_col.between?(0, 7)
+        piece = board[row][new_col]
+        return false if piece && piece.color == color # Blocked by own piece
+        if piece
+          # Check if the piece can attack horizontally (i.e., a rook or queen)
+          return piece.is_a?(Rook) || piece.is_a?(Queen)
+        end
+
+        new_col += col_step
+      end
+    end
+    false
+  end
+
+  def check_diagonals(board) # rubocop:disable Metrics/CyclomaticComplexity,Metrics/MethodLength
+    # Check diagonally from the king's current position
+    row, col = location
+
+    [[-1, -1], [-1, 1], [1, -1], [1, 1]].each do |row_step, col_step|
+      new_row = row + row_step
+      new_col = col + col_step
+      while new_row.between?(0, 7) && new_col.between?(0, 7)
+        piece = board[new_row][new_col]
+        return false if piece && piece.color == color # Blocked by own piece
+        if piece
+          # Check if the piece can attack diagonally (i.e., a bishop or queen)
+          return piece.is_a?(Bishop) || piece.is_a?(Queen)
+        end
+
+        new_row += row_step
+        new_col += col_step
+      end
+    end
+    false
+  end
+
+  def check_knights(board)
+    # Check the knight's movement range from the king's current position
+    row, col = location
+
+    knight_moves = [[-2, -1], [-2, 1], [2, -1], [2, 1], [-1, -2], [-1, 2], [1, -2], [1, 2]]
+    knight_moves.each do |row_step, col_step|
+      new_row = row + row_step
+      new_col = col + col_step
+      next unless new_row.between?(0, 7) && new_col.between?(0, 7)
+
+      piece = board[new_row][new_col]
+      # Check if the piece is an opponent's knight
+      return true if piece && piece.is_a?(Knight) && piece.color != color
+    end
+    false
   end
 end
